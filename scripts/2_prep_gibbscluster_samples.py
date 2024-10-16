e0991073f6b0f8f73ba87d50977e98c8c4bb42a5
apalkowski <aleksander.palkowski@gmail.com>
2024-10-16 15:21:59 +0200

    Add second step

---
+++ scripts/2_prep_gibbscluster_samples.py
@@ -0,0 +1,25 @@
+"""."""
+
+from pathlib import Path
+
+import pandas as pd
+
+project_dir = Path("/data/teamgdansk/apalkowski/carmen-analysis")
+
+data_dir = project_dir.joinpath("data")
+data_subs_dir = data_dir.joinpath("subsidiary-files")
+data_gibbs_dir = data_subs_dir.joinpath("gibbscluster")
+
+samples_peptides_dir = data_subs_dir.joinpath("samples-peptides")
+
+# split the files
+
+df = pd.read_csv("neo_uniq_sample_peptide_list.csv", sep="\t")
+
+grouped_by_sample = df.groupby(["Sample_name"])
+unique_keys = df["Sample_name"].unique()
+
+for unique_key in unique_keys:
+    grouped_by_sample.get_group(unique_key).to_csv(
+        samples_peptides_dir.joinpath(unique_key, ".csv"), index=False
+    )
