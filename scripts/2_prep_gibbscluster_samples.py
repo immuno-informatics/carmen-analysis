1eb0563b198fbf5e8d9f2b2a97baeb3786ba30e0
apalkowski <aleksander.palkowski@gmail.com>
2024-10-17 14:45:38 +0200

    Update

--- scripts/2_prep_gibbscluster_samples.py
+++ scripts/2_prep_gibbscluster_samples.py
@@ -8,8 +8,8 @@ project_dir = Path("/data/teamgdansk/apalkowski/carmen-analysis")
 
 data_dir = project_dir.joinpath("data")
 data_subs_dir = data_dir.joinpath("subsidiary-files")
-data_gibbs_dir = data_subs_dir.joinpath("gibbscluster")
 
+data_gibbs_dir = data_subs_dir.joinpath("gibbscluster")
 samples_peptides_dir = data_subs_dir.joinpath("samples-peptides")
 
 # split the files
