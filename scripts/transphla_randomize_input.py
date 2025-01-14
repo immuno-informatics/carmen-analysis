38994a4b8c679afd291e2fc38858626dd984e156
apalkowski <aleksander.palkowski@gmail.com>
2025-01-14 13:06:23 +0100

    Update scripts

--- scripts/transphla_randomize_input.py
+++ scripts/transphla_randomize_input.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
-# Main TransPHLA-AOMP directory
+# Root TransPHLA-AOMP directory
 transphla_dir = Path("/path/to/TransPHLA-AOMP")
 
 datasets_dir = transphla_dir.joinpath("Dataset")
